df81202b6288442e9d2fe3e020b6272e05e81c11
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-08-31 09:11:47 +0200

    update remove no longer needed files

--- Data/Setup/Update.php
+++ Data/Setup/Update.php
@@ -18,6 +18,25 @@ class Update
     protected $app = null;
     protected $Configuration = null;
 
+    protected function release_012()
+    {
+        $files = array(
+            '/miniShop/Template/default/command/include',
+            '/miniShop/Template/default/command/contact.order.twig',
+            '/miniShop/Template/default/command/list.article.twig',
+            '/miniShop/Template/default/command/order.twig',
+            '/miniShop/Template/default/command/view.article.twig',
+            '/miniShop/Template/default/command/view.basket.twig'
+        );
+        foreach ($files as $file) {
+            // remove no longer needed directories and files
+            if ($this->app['filesystem']->exists(MANUFAKTUR_PATH.$file)) {
+                $this->app['filesystem']->remove(MANUFAKTUR_PATH.$file);
+                $this->app['monolog']->addInfo(sprintf('[miniShop Update] Removed file or directory %s', $file));
+            }
+        }
+    }
+
     /**
      * Execute the update for the miniShop
      *
@@ -26,6 +45,9 @@ class Update
     public function Controller(Application $app)
     {
         $this->app = $app;
+
+        // Release 0.12
+        $this->release_012();
 
         return $app['translator']->trans('Successfull updated the extension %extension%.',
             array('%extension%' => 'miniShop'));
